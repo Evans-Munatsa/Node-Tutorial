714d1c1a35baafe43d590f1f87197e60151feb8a
Evans-Munatsa <gagamel.em@gmail.com>
2016-03-29 13:02:04 +0200

    all tests passing

--- node-tutorial/products.js
+++ node-tutorial/products.js
@@ -2,32 +2,43 @@ var fs = require('fs');
 
 module.exports = function(path) {
 
-  this.productNames = function(callback){
-    var list = fs.readFileSync(path, 'utf-8');
-    var items = list.split('\n');
-    var productList = [];
-    // var productSold = [];
+    this.productNames = function(callback) {
+        var list = fs.readFileSync(path, 'utf-8');
+        var items = list.split('\n');
+        var productList = [];
+        // var productSold = [];
 
-    items.forEach(function(items){
-      var productName = items.split(',')[1];
-      if (productList.indexOf(productName) === -1) {
-        productList.push(productName);
-      };
-    });
+        items.forEach(function(items) {
+            var productName = items.split(',')[1];
+            if (productList.indexOf(productName) === -1) {
+                productList.push(productName);
+            };
+        });
 
-    callback(null, productList);
-  };
+        callback(null, productList);
+    };
 
-  this.productsSold = function(callback) {
-    var list = fs.readFileSync(path, 'utf-8')
-    console.log('this is the products', list)
+    this.productsSold = function(callback) {
+        var list = fs.readFileSync(path, 'utf-8');
+        //array
+        var items = list.split('\n');
+        var quantityMap = {};
 
-     //var items = list.split('\n');
-  };
+        items.forEach(function(item) {
+            var columns = item.split(','); //want to get strings in array
+            //define your products 1 index
+            var products = columns[1];
+            var quantity = columns[2];
 
+            if (quantityMap[products] === undefined) {
+                quantityMap[products] = 0; //initializing to 0
+                // productSold.push(productName);
+            }
+            quantityMap[products] = quantityMap[products] + Number(quantity);
+            // console.log('this is the products', list);
+        });
+        //var items = list.split('\n');
+        console.log(quantityMap);
+        callback(null, quantityMap);
+    };
 };
-
-// this.productsSold = function(callback) {
-//   var list = fs.readFile('./files/products.csv', 'utf-8')
-//   var items = list.split('\n');
-// };
